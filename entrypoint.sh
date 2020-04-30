0c6974bd8eed8012fb86b84fac3ddbb6b5af4f9f
Samuel El-Borai <samuel.elborai@gmail.com>
2020-04-30 19:59:02 +0200

    Dockerfile and entrypoint script for dscanner

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,4 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -efm="%f:%l:%c: %m" -name="linter-name (misspell)" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+dub lint --quiet -- | reviewdog -efm="%f(%l:%c)[%tarn]: %m","%f(%l:%c)[%trror]: %m" -name="dscanner" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
