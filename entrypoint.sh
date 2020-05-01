5a2940ad766cb1ca4146a23e904c1ce6f44cf10d
Samuel El-Borai <samuel.elborai@gmail.com>
2020-05-01 11:22:25 +0200

    Debug dub lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,6 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
-if [ -n "${INPUT_PATH}" ]; then
-  cd "${INPUT_PATH}" || exit
-else
-  echo "INPUT_PATH not set"
-fi
-
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-dub lint --quiet | reviewdog -efm="%f(%l:%c)[%tarn]: %m" -efm="%f(%l:%c)[%trror]: %m" -name="dscanner" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+dub lint --root="${INPUT_PATH:-PWD}" | reviewdog -efm="%f(%l:%c)[%tarn]: %m" -efm="%f(%l:%c)[%trror]: %m" -name="dscanner" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
