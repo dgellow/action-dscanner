4dab0d323032f8a2b0193e4d3206e9f7296baa03
Samuel El-Borai <samuel.elborai@gmail.com>
2020-05-01 11:09:53 +0200

    Add an input path to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ if [ -n "${GITHUB_WORKSPACE}" ]; then
   cd "${GITHUB_WORKSPACE}" || exit
 fi
 
+if [ -n "${INPUT_PATH}" ]; then
+  cd "${INPUT_PATH}" || exit
+fi
+
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 dub lint --quiet | reviewdog -efm="%f(%l:%c)[%tarn]: %m" -efm="%f(%l:%c)[%trror]: %m" -name="dscanner" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
