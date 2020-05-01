6ba15c3ecfdcc1249656fc586d1496220177f636
Samuel El-Borai <samuel.elborai@gmail.com>
2020-05-01 10:11:06 +0200

    Add linter issues as test

--- testdata/hello.d
+++ testdata/hello.d
@@ -1,5 +1,19 @@
 import std.stdio;
+
+// Issue: Prefer the new "'alias' identifier '=' type ';'" syntax to the  old "'alias' type identifier ';'" syntax.
+alias string myString;
+
+// Issue: Empty declaration
+;
+
 void main(string[] args)
 {
-	writeln("Hello World");
+	try
+	{
+		writeln("Hello World");
+	}
+	// Issue: Catching Error or Throwable is almost always a bad idea.
+	catch (Throwable)
+	{
+	}
 }
