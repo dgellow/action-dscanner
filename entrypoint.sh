0c3517c22386ee339ea60e90a7ea03cf12fb39fa
Samuel El-Borai <samuel.elborai@gmail.com>
2020-05-01 10:44:45 +0200

    Fix syntax for multiple -efm arguments

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-dub lint --quiet | reviewdog -efm="%f(%l:%c)[%tarn]: %m","%f(%l:%c)[%trror]: %m" -name="dscanner" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
+dub lint --quiet | reviewdog -efm="%f(%l:%c)[%tarn]: %m" -efm="%f(%l:%c)[%trror]: %m" -name="dscanner" -reporter="${INPUT_REPORTER:-github-pr-check}" -level="${INPUT_LEVEL}"
